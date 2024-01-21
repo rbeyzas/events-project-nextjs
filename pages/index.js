98ceae3172a35773d94c34c08e927926c2788b5c
rbeyzas <rukiyebeyzasarikaya@hotmail.com>
2024-01-21 17:59:15 +0300

    home page created

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import EventList from '@/components/events/EventList';
+import { getFeaturedEvents } from '../dummy-data';
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>Welcome to Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
