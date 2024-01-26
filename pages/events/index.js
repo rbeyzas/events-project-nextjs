f48b70c1ebe5dc6c0cf2c67f969064836f8d53b9
rbeyzas <rukiyebeyzasarikaya@hotmail.com>
2024-01-26 20:04:46 +0300

    Add event search functionality

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,24 @@
 import EventList from '@/components/events/EventList';
 import { getAllEvents } from '../../dummy-data';
+import EventsSearch from '@/components/events/EventsSearch';
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
 function AllEventsPage() {
-  const featuredEvents = getAllEvents();
+  const router = useRouter();
+
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
   );
 }
 export default AllEventsPage;
