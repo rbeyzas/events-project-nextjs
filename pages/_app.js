bd4d54a656bdf81993266a54c0072de02df523b3
rbeyzas <rukiyebeyzasarikaya@hotmail.com>
2024-02-10 20:07:22 +0300

    react context added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
+/* eslint-disable import/no-unresolved */
 import Layout from '@/components/Layout/Layout';
 import '../styles/globals.css';
+import Notification from '@/components/Notifications/Notification';
+import { NotificationContextProvider } from '@/store/NotificationContext';
 
 function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+        <Notification title="Test" message="Test" status="success" />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
